873376cc959ffea6c76bf4e864db962b1786df4c
Moshe Malatji <38424146+MosheMalatji@users.noreply.github.com>
2019-08-21 13:15:44 +0200

    Add files via upload

---
+++ TaxCalculator/TaxCalculator/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TaxCalculator.Services;
+using TaxCalculator.Views;
+
+namespace TaxCalculator
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
